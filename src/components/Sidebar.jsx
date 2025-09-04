6394d97dad1317f4b9b0cd676f1f7eb367be4283
MrGodgames <sashka.bog14@gmail.com>
2025-09-04 10:18:51 +0300

    sidebar-header

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,17 @@ function Sidebar() {
 
   return (
     <>
-      <div className='sidebar'>тест</div>
+      <div className='sidebar'>
+        <div className='sidebar-header'>
+            <div className='sidebar-logo'>
+                <img src="" alt="" />
+            </div>
+            <div className='sidebar-logotext'>
+                <h1>Space-Point</h1>
+                <p>Космическая сеть</p>
+            </div>
+        </div>
+      </div>
     </>
   )
 }
