7457cef7dcb6273c83581e7b2014f6ed7930427e
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-09-16 11:33:52 +0300

    regpage

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
 import App from './App.jsx'
 import Chat from './Chat.jsx';
+import Register from './Register.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
             <Routes>
                 <Route path='/' element={<App />}/>
                 <Route path='/Chat' element={<Chat />}/>
+                <Route path='/Register' element={<Register />}/>
             </Routes>
         </BrowserRouter>
     </StrictMode>
